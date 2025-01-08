a2dc18fb24ad1b787a4c6c0ccfa799b3c1c17c75
Tarissa607 <tarissaeka88@gmail.com>
2025-01-08 13:02:25 +0700

    Add files via upload

---
+++ tugas tarisa.js
@@ -0,0 +1,21 @@
+let nama = "Tarissa Eka Wulandari";
+let alamat = "kudu baru Rt11/Rw07";
+let usia = "16";
+let ttl = "Semarang,29 Desember 2008";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "tarissaeka88";
+let berat = "57kg";
+let tinggi = "160cm";
+let hobi = "membaca novel dan nonton cdrama/kdrama";
+let cita = "menjadi CEO";
+let sts = "pelajar";
+let sekolah = "SMK NEGERI 1 SAYUNG";
+let jurusan = "pplg";
+dokument.write (" Nama Saya adalah " + nama +  " saya tinggal di " + alamat + " pada tahun 2025 ini saya usia + usia + " saya lahir di " + ttl + " jenis kelamin saya adalah " + gender 
+    + " saya beragama " + agama + " alamat email saya " + email + " saya memiliki berat badan " +  berat + " dengan tinggi " + tinggi 
+    + " saya sangat senang " + hobi + " karena saya mmemiliki cita cita  " + " saat ini saya adalah seorang " + sts +" yang bersekolah di " + " pada jurusan " +jurusan
+     ) 
+    
+    
+
